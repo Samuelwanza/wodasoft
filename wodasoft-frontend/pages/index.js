6d172c2ecea28c033af93bb150deccbc8fa22603
Samuel wanza <s.wanza@alustudent.com>
2023-01-30 21:39:39 +0200

    updates

--- wodasoft-frontend/pages/index.js
+++ wodasoft-frontend/pages/index.js
@@ -3,12 +3,13 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Task } from "../src/Components/TaskComponents";
 import React from "react";
+import { data } from "../src/Components/TaskComponents/Task";
 
 export default function Home() {
   return (
     <React.Fragment>
       <div>
-        <Task />
+        <Task data={data} />
       </div>
     </React.Fragment>
   );
--- wodasoft-frontend/src/Components/TaskComponents/Task.jsx
+++ wodasoft-frontend/src/Components/TaskComponents/Task.jsx
@@ -1,7 +1,441 @@
 import React from "react";
+import Link from "next/link";
+export const data = [
+  {
+    order_no: "Order No.1",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "Adopted",
+    hasButton: true,
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.2",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "In Progress",
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.3",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "Adopted ",
+    hasButton: true,
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.4",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "Adopted ",
+    hasButton: true,
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.5",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "In Progress",
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.6",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "Adopted ",
+    hasButton: true,
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.7",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "Adopted ",
+    hasButton: true,
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.8",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "In Progress",
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.9",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "Adopted ",
+    hasButton: true,
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.10",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "In Progress",
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.11",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "In Progress",
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.12",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "Adopted ",
+    hasButton: true,
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.13",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "In Progress",
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.14",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "In Progress",
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+  {
+    order_no: "Order No.15",
+    order_date: "01.01.2023",
+    amount: "100000 rubles",
+    status: "Adopted ",
+    hasButton: true,
+    moreinformation: [
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+      {
+        report_no: "Report 1. Tax Benefits",
+        amount: "100000 rubles",
+        hasLink: "Details",
+      },
+    ],
+  },
+];
 
-const Task = () => {
-  return <div>Task</div>;
+const Task = ({ data }) => {
+  const [expandedRow, setExpandedRow] = React.useState(-1);
+
+  return (
+    <div className="flex justify-center bg-white font-Nunito p-8">
+      <div className=" bg-white w-[80%] shadow-inner border-none border-grey-900">
+        <h1 className="text-center text-2xl text-secondary py-6 antialiased ">
+          Order Information
+        </h1>
+        <table className="table-auto bg-white w-[100%] shadow-inner border-none border-grey-900">
+          <thead className="bg-secondary text-white text-xl text-opacity-80 text">
+            <tr>
+              <th className="px-4 py-2 h-16">Orders</th>
+              <th className="px-4 py-2-16">Order date</th>
+              <th className="px-4 py-2 h-16">Amount</th>
+              <th className="px-4 py-2 h-16">Status</th>
+            </tr>
+          </thead>
+          <tbody className=" border-none border-grey text-grey-900">
+            {data.map((row, i) => (
+              <React.Fragment key={i}>
+                <tr
+                  className={`${
+                    i === expandedRow ? "bg-gray-200" : "bg-white"
+                  } cursor-pointer`}
+                  onClick={() => {
+                    if (row.hasButton) return;
+                    setExpandedRow(i === expandedRow ? -1 : i);
+                  }}
+                >
+                  <td className="border px-4 py-2 border-greyLight">
+                    {row.order_no}
+                  </td>
+                  <td className="border px-4 py-2  border-greyLight ">
+                    {row.order_date}
+                  </td>
+                  <td className="border px-4 py-2  border-greyLight ">
+                    {row.amount}
+                  </td>
+
+                  <td className="border px-4 py-2  border-greyLight">
+                    {row.status}
+                    {row.hasButton ? (
+                      <div className="mr-2 float-right">
+                        <button className="bg-secondary w-32 p-2 rounded-lg text-white text-sm ">
+                          To Pay
+                        </button>
+                      </div>
+                    ) : (
+                      <div className="mr-2 float-right ">
+                        <h3 className="text-sm">collated</h3>
+                      </div>
+                    )}
+                  </td>
+                </tr>
+                {i === expandedRow && (
+                  <>
+                    {row.moreinformation.map((info, i) => (
+                      <tr className="">
+                        <td className="border border-dashed px-4 py-2 border-greyLight border-b-secondary border-l-white border-r-white border-opacity-40  ">
+                          {info.report_no}
+                        </td>
+                        <td className="border border-dashed px-4 py-2 border-greyLight border-b-secondary border-l-white border-r-white border-opacity-40"></td>
+                        <td className="border border-dashed px-4 py-2 border-greyLight border-b-secondary  border-l-white border-r-white border-opacity-40">
+                          {info.amount}
+                        </td>
+                        <td className="border border-dashed px-4 py-2 border-greyLight border-b-secondary  border-l-white border-r-white border-opacity-40 text-secondary">
+                          <Link href="#">{info.hasLink}</Link>
+                        </td>
+                      </tr>
+                    ))}
+                  </>
+                )}
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Task;
